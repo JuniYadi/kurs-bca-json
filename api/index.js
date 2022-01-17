a2f1fc1c702034c51793790117482185570edf15
JuniYadi <juniyadi94@gmail.com>
2022-01-17 14:51:00 +0700

    Initial API

---
+++ api/index.js
@@ -0,0 +1,28 @@
+const app = require('express')();
+const BCA = require('@premiumfastnet/bca-kurs');
+
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+        res.status(200).end()
+        return
+    }
+    return await fn(req, res)
+}
+
+app.get('/api/kurs', async (req, res) => {
+    const currency = new BCA();
+    const data = await currency.getCurrency();
+
+    res.json(data);
+})
+
+module.exports = allowCors(app);
