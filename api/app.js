8e47cca02d9c317a7d6d3cf7cdc652c89294ec1a
JuniYadi <juniyadi94@gmail.com>
2022-08-09 10:08:47 +0700

    Update Disable Debug

--- api/app.js
+++ api/app.js
@@ -30,8 +30,6 @@ const dateParser = (date) => {
     const dateParse = `${day}-${monthConv[month.toLowerCase()]}-${year} ${hour}:${minute}:00`;
     const dateObj = new Date(dateParse);
 
-    console.log(dateObj)
-
     const dateUTC = dateObj.toISOString();
     const dateTimestamp = +new Date(dateParse);
 
@@ -45,8 +43,6 @@ const dateParser = (date) => {
 app.get('/api/kurs', async (req, res) => {
     const currency = new BCA();
     const datas = await currency.getCurrency();
-
-    console.log(datas)
 
     // date parser
     const dateConvert = dateParser(datas.check);
